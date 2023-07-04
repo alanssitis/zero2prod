95036b715086be81af6473a8ec804a4398b75180
alanssitis <alanchunggt@gmail.com>
2023-07-03 22:46:57 -0700

    added database utility and functionality

--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,18 @@
 use crate::routes;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // Wrap connection in a smart pointer
+    let connection = web::Data::new(db_pool);
+    // Capture 'connection' from surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
