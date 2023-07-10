3e9d27d3bb471982fe414814b7e29fea8e393f15
alanssitis <alanchunggt@gmail.com>
2023-07-09 21:36:38 -0700

    added basic logging

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,6 +10,13 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber in the database",
+        request_id,
+        form.email,
+        form.name
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -23,9 +30,19 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}!",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,8 @@
 use crate::routes;
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpServer};
+use actix_web::{App, HttpServer};
+use actix_web::middleware::Logger;
+use actix_web::web;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -10,6 +12,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     // Capture 'connection' from surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
             .app_data(connection.clone())
